51956b20b128ff49e4920797865b1cfafde4ae8a
Rob Watson <rob@netflux.io>
2025-01-18 14:42:26 +0100

    feat: bootstrap server

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"git.netflux.io/rob/termstream/container"
+	"git.netflux.io/rob/termstream/mediaserver"
+)
+
 func main() {
-	println("Hello, World!")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		_, _ = os.Stderr.WriteString("Error: " + err.Error() + "\n")
+	}
+}
+
+func run(ctx context.Context) error {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	runner, err := container.NewRunner(logger.With("component", "runner"))
+	if err != nil {
+		return fmt.Errorf("new runner: %w", err)
+	}
+	defer runner.Close()
+
+	srv, err := mediaserver.StartActor(ctx, mediaserver.StartActorParams{
+		Runner: runner,
+		Logger: logger.With("component", "mediaserver"),
+	})
+	if err != nil {
+		return fmt.Errorf("start media server: %w", err)
+	}
+
+	for {
+		select {
+		case <-ch:
+			logger.Info("Received interrupt signal, shutting down...")
+			return nil
+		case state, ok := <-srv.C():
+			if ok {
+				logger.Info("Received state change", "state", state)
+			} else {
+				logger.Info("State channel closed, shutting down...")
+				return nil
+			}
+		}
+	}
 }
